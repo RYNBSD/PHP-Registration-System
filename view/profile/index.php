1365d1d4fad9b0ef81e50e783d54c290c1590d5a
RYN BSD <rynbsd04@gmail.com>
2024-11-18 04:19:40 +0100

    add bootstrap in portfolio

--- view/profile/index.php
+++ view/profile/index.php
@@ -16,11 +16,13 @@ print_r(auth());
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Profile</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body>
   <form method="post" action="./sign-out.php">
-    <button type="submit">Sign out</button>
+    <button type="submit" class="btn btn-primary">Sign out</button>
   </form>
 </body>
 
