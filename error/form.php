a2cb2f6e776d6d291d5e055c33351b2c6ade666b
RYN BSD <rynbsd04@gmail.com>
2024-11-24 12:42:04 +0100

    remove parameter name in form error

--- error/form.php
+++ error/form.php
@@ -4,7 +4,7 @@ $error = array_key_exists("error", $_GET) ?
   htmlspecialchars((string)$_GET["error"])
   : null;
 $message = array_key_exists("message", $_GET) ?
-  htmlspecialchars(urldecode(string: (string)$_GET["message"]))
+  htmlspecialchars(urldecode((string)$_GET["message"]))
   : null;
 
 function error_form_script()
